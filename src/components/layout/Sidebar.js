70ff40d1fefdfb33ded24d31532b302d50c1858f
Jeff <zjyeung579@gmail.com>
2020-08-25 12:38:39 -0700

    Setup Sidebar component structure

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa'
 
 const SidebarSection = styled('div')`
 
-`
+`;
+
+const SidebarList = styled('ul')`
+
+`; 
+
+const SidebarMiddle = styled('div')`
+`;
+
+const SidebarProjects = styled('ul')`
+`;
+
 
 const Sidebar = () => {
     return (
-        <SidebarSection>
-            <p>
-                I am a sidebar
-            </p>
+        <SidebarSection data-testid="sidebar">
+            <SidebarList>
+                <li>
+                    <span><FaInbox /></span>
+                    <span>Inbox</span>
+                </li>
+                <li>
+                    <span><FaRegCalendar /></span>
+                    <span>Today</span>
+                </li>
+                <li>
+                    <span><FaRegCalendarAlt /></span>
+                    <span>Next 7 days</span>
+                </li>
+            </SidebarList>
+
+            <SidebarMiddle>
+                <span><FaChevronDown /></span>
+                <h2>Projects</h2>
+            </SidebarMiddle>
+
+            <SidebarProjects>
+                Projects will be be here!
+            </SidebarProjects>
+            Add Project Component Here!
         </SidebarSection>
     )
 }
