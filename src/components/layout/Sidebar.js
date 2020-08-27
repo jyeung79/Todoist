02f59cb53eb3681271a8cae7eebe75f0bde525e9
Jeff <zjyeung579@gmail.com>
2020-08-27 16:58:43 -0700

    Setup sidebar layout with css and styling

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,35 +1,93 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa'
+import BaseStyles from '../../styles/styles';
 
-const SidebarSection = styled('div')`
+const SidebarLayout = styled('div')`
+    ${BaseStyles.no_select}
+    width: 266px;
+    height: calc(100vh);
+    padding-top: 74px;
+    position: fixed;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    border-right: ${BaseStyles.generic_border};
+    background-color: #fafafa;
 
+    li {
+        :hover .sidebar .project-delete {
+          display: -webkit-box;
+          display: -ms-flexbox;
+          display: flex;
+        }
+    }
 `;
 
 const SidebarList = styled('ul')`
+    li {
+        list-style-type: none;
+        color: #333;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        cursor: pointer;
+        line-height: 1.50;
+        font-size: 15px;
+        padding-left: 0;
+        padding-right: 0;
 
-`; 
+        div:nth-child(1) {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            padding: 10px 0 10px 10px;
+            width: 100%;
+
+                span:first-of-type {
+                    margin-right: 10px;
+                    display: -webkit-box;
+                    display: -ms-flexbox;
+                    display: flex;
+                    -webkit-box-align: center;
+                        -ms-flex-align: center;
+                            align-items: center;
+
+                        svg {
+                        width: 18px;
+                        height: 18px;
+                        color: #555;
+                        }
+                }
+        }
+        
+        .active, :hover {
+            font-weight: bold;
+            background-color: white;
+        }
+    }
+`;
 
 const SidebarMiddle = styled('div')`
 `;
 
 const SidebarProjects = styled('ul')`
+    padding-left: 3px;
 `;
 
 
 const Sidebar = () => {
     return (
-        <SidebarSection data-testid="sidebar">
+        <SidebarLayout data-testid="sidebar">
             <SidebarList>
-                <li>
+                <li data-testid="inbox">
                     <span><FaInbox /></span>
                     <span>Inbox</span>
                 </li>
-                <li>
+                <li data-testid="today">
                     <span><FaRegCalendar /></span>
                     <span>Today</span>
                 </li>
-                <li>
+                <li data-testid="next7">
                     <span><FaRegCalendarAlt /></span>
                     <span>Next 7 days</span>
                 </li>
@@ -44,7 +102,7 @@ const Sidebar = () => {
                 Projects will be be here!
             </SidebarProjects>
             Add Project Component Here!
-        </SidebarSection>
+        </SidebarLayout>
     )
 }
 
